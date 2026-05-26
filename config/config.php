0ddf10a7498eca978594c1f475df2cb9cdcf3ecc
darckshafow-wq <darckshafow@gmail.com>
2026-05-26 17:29:43 +0000

    Update db user to admin_scolaire

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 // Configuration de la base de données
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', ''); // Mettez votre mot de passe si vous en avez configuré un (souvent 'root' ou vide)
+define('DB_USER', 'admin_scolaire');
+define('DB_PASS', 'admin123');
 define('DB_NAME', 'gestion_scolaire');
